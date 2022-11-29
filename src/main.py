e63a9c239dd2f1f12fe54ed67ffe6d40d787db40
ChuchoAraujo <jeseazuara@gmail.com>
2022-11-29 14:50:58 +0000

    1er commit

--- src/main.py
+++ src/main.py
@@ -8,7 +8,8 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planets, Characters, Favorites
+import json
 #from models import Person
 
 app = Flask(__name__)
@@ -20,6 +21,7 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -30,14 +32,162 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+#-------------------------- LLAMADAS GET --------------------------------------------#
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+### GET PEOPLE
+@app.route('/people', methods=['GET'])
+def get_people():
+    callCharacters = Characters.query.all()
+    result = [element.serialize() for element in callCharacters]
+    response_body = {"message": "Todo ok bro"}
+    return jsonify(result), 200
 
-    return jsonify(response_body), 200
+
+### GET PLANETS
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    callPlanet = Planets.query.all()
+    result = [element.serialize() for element in callPlanet]
+    response_body = {"message": "Todo ok bro"}
+    return jsonify(result), 200
+
+
+### GET USERS
+@app.route('/users', methods=['GET'])
+def get_users():
+    callUsers = User.query.all()
+    result = [element.serialize() for element in callUsers]
+    response_body = {"message": "Todo ok bro"}
+    return jsonify(result), 200
+
+### GET FAVORITES
+@app.route('/favorites', methods=['GET'])
+def get_favorites():
+    callFavorites = Favorites.query.all()
+    result = [element.serialize() for element in callFavorites]
+    response_body = {"message": "Todo ok bro"}
+    return jsonify(result), 200
+
+#-------------------------- LLAMADAS GET ID --------------------------------------------#
+
+### GET PEOPLE ID
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_people_id(people_id):
+    callOneCharacter = Characters.query.get(people_id)
+    result = callOneCharacter.serialize()
+    response_body = {"message": "Todo ok bro"}
+    return jsonify(result), 200
+
+
+### GET PLANET ID
+@app.route('/planets/<int:planets_id>', methods=['GET'])
+def getOnePlanet(planets_id):
+    callPlanetOne = Planets.query.get(planets_id)
+    result = callPlanetOne.serialize()
+    response_body = {"message": "Todo ok bro"}
+    return jsonify(result), 200
+
+
+### GET USERS_ID
+@app.route('/users/<int:users_id>', methods=['GET'])
+def get_users_id(users_id):
+    callOneUser = User.query.get(users_id)
+    result = callOneUser.serialize()
+    response_body = {"message": "Todo ok bro"}
+    return jsonify(result), 200
+
+#-------------------------- LLAMADAS POST --------------------------------------------#
+### POST PLANETS
+@app.route('/planets', methods= ['POST'])
+def createPlanet():
+    data= request.data
+    data = json.loads(data)
+    planet = Planets(name = data["name"], id = data["id"], diameter = data["diameter"], description = data["description"])
+    db.session.add(planet)
+    db.session.commit()
+
+    response_body ={"msg": "ok"}
+    return jsonify(planet.serialize())
+
+
+### POST PEOPLE
+@app.route('/people', methods= ['POST'])
+def createCharacter():
+    data = request.data
+    data = json.loads(data)
+    character = Characters(name = data["name"], height = data["height"], gender = data["gender"], description = data["description"])
+    db.session.add(character)
+    db.session.commit()
+
+    response_body = {"msg": "recived"}
+    return jsonify(character.serialize())
+
+
+### POST USERS
+@app.route('/users', methods= ['POST'])
+def createUser():
+    data = request.data
+    data = json.loads(data)
+    newUser = User(name = data["name"], email = data["email"], id = data["id"], password = data["password"], is_active = data["is_active"])
+    db.session.add(newUser)
+    db.session.commit()
+
+    response_body = {"msg": "recived"}
+    return jsonify(newUser.serialize())
+
+
+
+#-------------------------- LLAMADAS DELETE --------------------------------------------#
+
+### DELETE PLANET
+@app.route('/planets/<int:planets_id>', methods= ['DELETE'])
+def deletePlanet(planets_id):
+    planet = Planets.query.get(planets_id)
+    db.session.delete(planet)
+    db.session.commit()
+
+    response_body = {"msg": "borrado"}
+    return jsonify(planet.serialize())
+
+
+### DELETE PEOPLE
+@app.route('/people/<int:people_id>', methods= ['DELETE'])
+def deletePeople(people_id):
+    character = Characters.query.get(people_id)
+    db.session.delete(character)
+    db.session.commit()
+
+    response_body = {"msg": "borrado"}
+    return jsonify(character.serialize())
+
+
+### DELETE PEOPLE
+@app.route('/users/<int:users_id>', methods= ['DELETE'])
+def deleteUser(users_id):
+    byeUser = User.query.get(users_id)
+    db.session.delete(byeUser)
+    db.session.commit()
+
+    response_body = {"msg": "borrado"}
+    return jsonify(byeUser.serialize())
+
+#-------------------------- LLAMADAS DELETE --------------------------------------------#
+
+### FAVORITES PLANETS
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
+def fav_planet(planet_id):
+    body_request = request.get_json()
+
+    planet_id = body_request.get("planet_id", None)
+
+    newplanetFav = Favorites()
+    db.session.add(newplanetFav)
+    db.session.commit()
+
+    response_body = {"msg": "Favorite add"}
+    return jsonify(response_body)
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -4,16 +4,73 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
             "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    description = db.Column(db.String(250))
+    diameter = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Planets %r>' % self.id
+    
+    def serialize(self):
+        return {
+            "id" : self.id,
+            "name": self.name,
+            "diameter": self.diameter,
+            "description": self.description
+        }
+
+
+class Characters(db.Model):
+    id = db.Column(db.Integer, primary_key= True)
+    name = db.Column(db.String(80))
+    description = db.Column(db.String(250))
+    gender = db.Column(db.String(80))
+    height = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Characters %r>' % self.id
+    
+    def serialize(self):
+        return {
+                "name": self.name,
+                "description": self.description,
+                "gender": self.gender,
+                "height": self.height
+        }
+
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    planet_id = db.Column(db.Integer, db.ForeignKey("planets.id"))
+    character_id = db.Column(db.Integer, db.ForeignKey("characters.id"))
+
+    def __repr__(self):
+        return '<Favorites %r>' % self.id 
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "planet_id": self.planet_id,
+            "character_id": self.character_id,
             # do not serialize the password, its a security breach
         }
